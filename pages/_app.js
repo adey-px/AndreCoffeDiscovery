0e2869c66f2cb491c572ab588a018aec65d8c285
adey-px <aosteph@gmail.com>
2023-10-08 23:40:00 -0500

    Added footer text to pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
 
+// Wrapper for all pages, add shared UI components
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<div>
+			<Component {...pageProps} />
+
+			<footer>
+				<p>@ 2023 Developer</p>
+			</footer>
+		</div>
+	);
 }
